c409ebababf003104a98da74cc1413c5c8614084
marcin.pierchala@icloud.com <marcin.pierchala@icloud.com>
2023-09-22 19:58:50 +0200

    Add project files.

---
+++ DesktopAppV_001/AboutMe.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DesktopAppV_001
+{
+    /// <summary>
+    /// Interaction logic for AboutMe.xaml
+    /// </summary>
+    public partial class AboutMe : Window
+    {
+        public AboutMe()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void btn_back_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ DesktopAppV_001/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace DesktopAppV_001
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            
+            InitializeComponent();
+            TextBox_01.Text = "";
+            TextBox_02.Text = "";
+            TB_03.Text = "";
+        }
+        public void SetLabel_01(string txt)
+        {
+            string str = txt;
+            TextBox_01.Text = txt;
+            
+        }
+
+        private void SetLabel_01(object sender, TextCompositionEventArgs e)
+        {
+            TextBox_01.Text = sender.ToString();
+        }
+
+        private void SetLabel_01_02(object sender, TextChangedEventArgs e)
+        {
+            //foreach(char c in TextBox_01.Text) { }
+         string str = TextBox_01.Text;
+            var str2 = str.ToCharArray().Reverse(); 
+            TextBox_02.Text = new string(str2.ToArray());
+            string str01 = TextBox_01.Text;
+            string str02 = TextBox_02.Text;
+            TB_03.Text = str01 + str02;
+        }
+
+        private void SetMirrorText_TB_03(object sender, TextChangedEventArgs e)
+        {
+            //string str01 = TextBox_01.Text;
+            //string str02 = TextBox_02.Text;
+            //TB_03.Text = str01 + str02;
+        }
+
+        private void btn_close_Click(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show("Are you sure to close the application?","Close_button",MessageBoxButton.YesNo,MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                this.Close();
+            }            
+        }
+
+        private void btn_about_Click(object sender, RoutedEventArgs e)
+        {            
+            AboutMe aboutMe = new AboutMe();
+            aboutMe.Show();
+        }
+
+        private void btn_clear_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox_01.Text = "";
+            //TextBox_02.Text = "";
+            //TB_03.Text = "";
+        }
+    }
+}
